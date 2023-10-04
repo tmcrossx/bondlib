ff153763c96f701260a17010982505156004380e
Keith A. Lewis <kal@kalx.net>
2023-10-04 10:09:05 -0400

    date

--- bondlib/tmx_date.h
+++ bondlib/tmx_date.h
@@ -1,199 +1,420 @@
-﻿// tmx_date.h - Date routines
+﻿// fms_date.h - Date and time calculation
 #pragma once
 #include <chrono>
+#include <iterator>
+#include <tuple>
 
-namespace tmx::date {
+namespace fms::date {
 
+	// Calendar date.
 	using ymd = std::chrono::year_month_day;
-	using std::chrono::sys_days;
 
-	// days per year conversion convention
-	using std::chrono::years;
-	constexpr double dpy = years::period::num / (86400. * years::period::den);
-
-	namespace frequency {
-		constexpr auto annually = 1;
-		constexpr auto semiannually = 2;
-		constexpr auto quarterly = 4;
-		constexpr auto monthly = 12;
+	constexpr ymd make_ymd(int y, unsigned int m, unsigned int d)
+	{
+		return ymd(std::chrono::year(y), std::chrono::month(m), std::chrono::day(d));
+	}
+	constexpr std::tuple<std::chrono::year, std::chrono::month, std::chrono::day> from_ymd(const ymd& d)
+	{
+		return { d.year(), d.month(), d.day() };
 	}
 
-	template<class Dur>
-	constexpr sys_days as_days(const std::chrono::sys_time<Dur>& t)
+	// Construct sys_days time point from ymd.
+	using sys_days = std::chrono::sys_days;
+	constexpr sys_days make_days(int y, int m, int d)
 	{
-		return std::chrono::time_point_cast<std::chrono::days>(t);
-	}
-	template<class Dur>
-	constexpr ymd as_ymd(const std::chrono::sys_time<Dur>& t)
-	{
-		return std::chrono::time_point_cast<ymd>(t);
+		// ymd has operator operator sys_days conversion
+		return sys_days(make_ymd(y, m, d));
 	}
 
-	template<class Clk, class Dur>
-	constexpr auto add_years(const std::chrono::time_point<Clk, Dur>& t, double y)
+	// duration as double in years
+	using years = std::chrono::duration<double, std::chrono::years::period>;
+
+	// d0 - d1 in years to system clock precision.
+	// If dt = d0 - d1 then d1 = d0 - dt and d0 = d1 + dt.
+	constexpr years operator-(ymd d0, ymd d1)
 	{
-		return t + std::chrono::seconds{ static_cast<long long>(y * dpy * 86400) };
-	}
-	template<class Clk, class Dur>
-	constexpr double sub_years(const std::chrono::time_point<Clk, Dur>& t1, const std::chrono::time_point<Clk, Dur>& t0, double dpy = date::dpy)
-	{
-		return std::chrono::duration_cast<std::chrono::seconds>(t1 - t0).count()/(dpy*86400);
+		return years(sys_days(d0) - sys_days(d1));
 	}
 
 #ifdef _DEBUG
-
-#pragma warning(push)
-#pragma warning(disable: 4455)
-	inline int years_test()
+	int basic_date_test()
 	{
-		using std::literals::chrono_literals::operator""y;
-		using std::chrono::years;
-		using std::chrono::months;
-		using std::chrono::days;
-		using std::chrono::sys_days;
-		using std::chrono::sys_time;
-
 		{
-			// ymd to time_point
-			constexpr sys_days t0{ 2023y / 1 / 1 };
-			constexpr sys_days t1{ 2023y / 1 / 2 };
-
-			static_assert(sub_years(t0, t0) == 0);
-			static_assert(sub_years(t1, t0) == 1 / dpy);
-
-			//!!! more tests
-
-			// sub_years can be negative
-			static_assert(sub_years(t0, t1) == -1 / dpy);
+			constexpr auto d0 = make_ymd(2023, 4, 5);
+			constexpr auto d1 = make_ymd(2024, 4, 5);
+			constexpr auto dd = d0 - d1;
+			static_assert(d1 - std::chrono::years(1) == d0);
+			static_assert(sys_days(d1) + dd == sys_days(d0));
+			static_assert(sys_days(d0) - dd == sys_days(d1));
 		}
 		{
-			constexpr sys_days t{ 2023y / 1 / 1 };
-			constexpr auto t0 = add_years(t, 1.1);
-			constexpr auto t1 = add_years(t0, -1.1);
-			constexpr auto dt = (t - t1).count();
-			static_assert(0 == dt);
-
-			/*
-			constexpr auto t2 = t + std::chrono::years(1);
-			constexpr auto t2_ = sys_days{ 2024y / 1 / 1 };
-			constexpr auto dt = t2 - sys_days{2024y / 1 / 1};
-			//static_assert(sys_days{ t2 } == sys_days{2024y / 1 / 1});
-			*/
+			constexpr auto d0 = make_ymd(2023, 4, 5);
+			constexpr auto d1 = make_ymd(2024, 6, 7);
+			constexpr auto dd = d0 - d1;
+			static_assert(sys_days(d1) + dd == sys_days(d0));
+			static_assert(sys_days(d0) - dd == sys_days(d1));
+		}
+		{
+			constexpr auto d0 = make_ymd(2023, 4, 5);
+			constexpr auto d1 = make_ymd(2024, 7, 6);
+			constexpr auto dd = d0 - d1;
+			static_assert(sys_days(d1) + dd == sys_days(d0));
+			static_assert(sys_days(d0) - dd == sys_days(d1));
 		}
 
 		return 0;
 	}
-#pragma warning(pop)
-
 #endif // _DEBUG
 
-	// Day Count Fractions
-	using dcf_t = double(const ymd&, const ymd&);
+	// Periodic times in [effective, termination] working backwards from termination in period steps.
+	class periodic {
+		ymd effective, termination;
+		int months;
+		ymd current;
+	public:
+		// STL iterator
+		using iterator_category = std::forward_iterator_tag;
+		using value_type = ymd;
 
-	constexpr double dcf_years(const ymd& d0, const ymd& d1)
-	{
-		return sub_years(sys_days{ d1 }, sys_days{ d0 }, dpy);
-	}
-	constexpr double dcf_actual_360(const ymd& d0, const ymd& d1)
-	{
-		return sub_years(sys_days{ d1 }, sys_days{ d0 }, 360);
-	}
-	constexpr double dcf_actual_365(const ymd& d0, const ymd& d1)
-	{
-		return sub_years(sys_days{ d1 }, sys_days{ d0 }, 365);
-	}
-
-	// 2006-isda-definitions.pdf
-	constexpr double dcf_actual_actual(const ymd& d0, const ymd& d1)
-	{
-		return sub_years(sys_days{ d1 }, sys_days{ d0 }); //!!! not correct
-	}
-
-	//!!! tests
-	// check with y/2/29 endpoints
-
-#ifdef _DEBUG
-
-#pragma warning(push)
-#pragma warning(disable: 4455)
-	inline int dcf_actual_actual_test()
-	{
-		using std::literals::chrono_literals::operator""y;
-		using std::chrono::sys_days;
+		constexpr periodic(ymd effective, ymd termination, int months, bool end = false)
+			: effective{ effective }, termination{ termination }, months{ months }, current{ termination }
 		{
-			constexpr sys_days t0{ 2023y / 1 / 1 };
-			constexpr sys_days t1{ 2023y / 1 / 2 };
-			static_assert(dcf_actual_actual(t0, t0) == 0);
-			static_assert(dcf_actual_actual(t0, t1) == 1 / dpy);
+			if (end) {
+				operator++();
+			}
+			else if (valid()) {
+				reset();
+			}
+		}
+		constexpr periodic(const periodic&) = default;
+		constexpr periodic& operator=(const periodic&) = default;
+		constexpr ~periodic() = default;
 
-			/*
-			logical error? there are 367 days between the two dates but adding the leap day changes that to 368
-			Question: shouldn't sys_days .count() already factor in leap days?
-			and instead shouldn't we split the years as given in the 2006-isda pdf :
+		constexpr bool operator==(const periodic&) const = default;
 
-			if “Actual/Actual”, “Actual/Actual (ISDA)”, “Act/Act” or “Act/Act (ISDA)” is specified,
-			the actual number of days in the Calculation Period or Compounding Period in respect of which payment
-			is being made divided by 365 (or, if any portion of that Calculation Period or Compounding Period falls
-			in a leap year, the sum of (i) the actual number of days in that portion of the Calculation Period or
-			Compounding Period falling in a leap year divided by 366 and (ii) the actual number of days in that
-			portion of the Calculation Period or Compounding Period falling in a non-leap year divided by 365)
+		// order and direction of period are compatible
+		constexpr bool valid() const
+		{
+			if (effective == termination) {
+				return months == 0;
+			}
 
-			*/
-			//static_assert(dcf_actual_actual(sys_days{ 2023y / 2 / 28 }, sys_days{ 2024y / 3 / 1 }) == 368 / 365.);
-			//static_assert(dcf_actual_actual(sys_days{2024y / 2 / 28}, sys_days{2024y / 2 / 29}) == 2 / 365.);
-			//!!! more tests
+			return (effective < termination) == (months > 0);
+		}
+
+		constexpr auto begin() const
+		{
+			return periodic(effective, termination, months);
+		}
+		constexpr auto end() const
+		{
+			periodic(effective, termination, months, true);
+		}
+
+		constexpr explicit operator bool() const
+		{
+			return current <= termination;
+		}
+		constexpr value_type operator*() const
+		{
+			return current;
+		}
+		constexpr periodic& operator++()
+		{
+			if (*this) {
+				current += std::chrono::months(months);
+			}
+
+			return *this;
+		}
+	protected:
+		constexpr void reset()
+		{
+			while (current - std::chrono::months(months) >= effective) {
+				current -= std::chrono::months(months);
+			}
+		}
+	};
+#ifdef _DEBUG
+	static int periodic_test()
+	{
+		using year = std::chrono::year;
+		{
+			constexpr auto eff = make_ymd(2023, 1, 2);
+			constexpr auto ter = make_ymd(2025, 1, 2);
+			constexpr auto pi = periodic(eff, ter, 12);
+			static_assert(pi);
+			constexpr auto pi2{ pi };
+			static_assert(pi2 == pi);
+			constexpr auto pi3 = pi2;
+			static_assert(!(pi3 != pi2));
+			static_assert(eff == *pi3);
+		}
+		{
+			constexpr auto eff = make_ymd(2023, 1, 1);
+			constexpr auto ter = make_ymd(2025, 1, 2);
+			constexpr auto pi = periodic(eff, ter, 12);
+			static_assert(*pi == make_ymd(2023, 1, 2));
+		}
+		{
+			constexpr auto eff = make_ymd(2023, 1, 3);
+			constexpr auto ter = make_ymd(2025, 1, 2);
+			constexpr auto pi = periodic(eff, ter, 12);
+			static_assert(*pi == make_ymd(2024, 1, 2));
+		}
+		{
+			constexpr auto eff = make_ymd(2023, 1, 1);
+			constexpr auto ter = make_ymd(2025, 2, 1);
+			constexpr auto pi = periodic(eff, ter, 12);
+			static_assert(*pi == make_ymd(2023, 2, 1));
+		}
+		{
+			constexpr auto eff = make_ymd(2023, 3, 1);
+			constexpr auto ter = make_ymd(2025, 2, 1);
+			constexpr auto pi = periodic(eff, ter, 12);
+			static_assert(*pi == make_ymd(2024, 2, 1));
+		}
+		{
+			auto d0 = make_ymd(2023, 4, 5);
+			auto d1 = make_ymd(2025, 4, 5);
+			auto pi = periodic(d0, d1, 12);
+			ymd ps[3];
+			ps[0] = *pi;
+			assert(ps[0] == d0);
+			ps[1] = *++pi;
+			assert(ps[1] == d0 + std::chrono::years(1));
+			assert(ps[1] == d1 - std::chrono::years(1));
+			ps[2] = *++pi;
+			assert(ps[2] == d1);
+			++pi;
+			assert(!pi);
 		}
 
 		return 0;
 	}
-#pragma warning(pop)
-
 #endif // _DEBUG
 
-	// 2006-isda-definitions.pdf 
-	constexpr double dcf_30_360(const ymd& d0, const ymd& d1)
+	// Day count fraction appoximately equal to time in year between dates.
+	// https://eagledocs.atlassian.net/wiki/spaces/Accounting2017/pages/439484565/Understand+Day+Count+Basis+Options
+	using dcf_ = years(*)(const ymd&, const ymd&);
+	namespace dcf {
+		// Day count fraction in years from d0 to d1.
+		constexpr years _years(const ymd& d0, const ymd& d1)
+		{
+			return d1 - d0;
+		}
+		// NASD
+		constexpr years _30_360(const ymd& t0, const ymd& t1)
+		{
+			using std::chrono::day;
+			auto d0 = (t0.day() == day(31))
+				? day(30)
+				: t0.day();
+			auto d1 = (t1.day() == day(31) and d0 > day(29))
+				? day(30)
+				: t1.day();
+
+			int dy = (int)t1.year() - (int)t0.year();
+			int dm = (unsigned)t1.month() - (unsigned)t0.month();
+			int dd = (unsigned)d1 - (unsigned)d0;
+
+			return years(360 * dy + 30 * dm + dd) / 360.;
+		}
+		// ISMA
+		constexpr years _30E_360(const ymd& t0, const ymd& t1)
+		{
+			using std::chrono::day;
+			auto d0 = (t0.day() == day(31))
+				? day(30)
+				: t0.day();
+			auto d1 = (t1.day() == day(31))
+				? day(30)
+				: t1.day();
+
+			int dy = (int)t1.year() - (int)t0.year();
+			int dm = (unsigned)t1.month() - (unsigned)t0.month();
+			int dd = (unsigned)d1 - (unsigned)d0;
+
+			return years(360 * dy + 30 * dm + dd) / 360.;
+		}
+		constexpr years _actual_360(const ymd& t0, const ymd& t1)
+		{
+			return years((sys_days(t1) - sys_days(t0)).count() / 360.);
+		}
+		constexpr years _actual_365(const ymd& t0, const ymd& t1)
+		{
+			return years((sys_days(t1) - sys_days(t0)).count() / 365.);
+		}
+
+#ifdef _DEBUG
+#define DATE_DCF_TEST(X) \
+	X(make_ymd(2003, 12, 29), make_ymd(2004, 1, 31), 31, 32, 33) \
+	X(make_ymd(2003, 12, 30), make_ymd(2004, 1, 31), 30, 30, 32) \
+	X(make_ymd(2003, 12, 31), make_ymd(2004, 1, 31), 30, 30, 31) \
+	X(make_ymd(2004, 1, 1), make_ymd(2004, 1, 31), 29, 30, 30) \
+	X(make_ymd(2003, 12, 29), make_ymd(2004, 2, 1), 32, 32, 34) \
+	X(make_ymd(2003, 12, 30), make_ymd(2004, 2, 1), 31, 31, 33) \
+	X(make_ymd(2003, 12, 31), make_ymd(2004, 2, 1), 31, 31, 32) \
+	X(make_ymd(2004, 1, 1), make_ymd(2004, 2, 1), 30, 30, 31) \
+
+		static int test()
+		{
+#define TEST_DATE_DCF(d0, d1, a, b, c) static_assert(dcf::_30E_360(d0, d1) == years(a/360.));
+			DATE_DCF_TEST(TEST_DATE_DCF)
+#undef TEST_DATE_DCF
+#define TEST_DATE_DCF(d0, d1, a, b, c) static_assert(dcf::_30_360(d0, d1) == years(b/360.));
+				DATE_DCF_TEST(TEST_DATE_DCF)
+#undef TEST_DATE_DCF
+#define TEST_DATE_DCF(d0, d1, a, b, c) static_assert(dcf::_actual_360(d0, d1) == years(c/360.));
+				DATE_DCF_TEST(TEST_DATE_DCF)
+#undef TEST_DATE_DCF
+				using year = std::chrono::year;
+			{
+				constexpr auto t0 = year(2023) / 1 / 2;
+				constexpr auto t1 = year(2024) / 1 / 2;
+				constexpr auto y2 = dcf::_30_360(t0, t1);
+				static_assert(y2 == years(1));
+				constexpr auto y3 = dcf::_actual_360(t0, t1);
+				constexpr auto dd = sys_days(t1) - sys_days(t0);
+				static_assert(dd == std::chrono::days(365));
+				constexpr auto yy = years(dd.count() / 360.);
+				static_assert(y3 == yy);// years(dd.count() / 360.));
+			}
+			{
+				constexpr auto t0 = year(2023) / 1 / 2;
+				constexpr auto t1 = year(2024) / 1 / 4;
+				constexpr auto y2 = dcf::_30_360(t0, t1);
+				constexpr auto y2_ = years(1 + 0 / 30. + 2 / 360.);
+				static_assert(y2 == y2_);
+				constexpr auto y3 = dcf::_actual_365(t0, t1);
+				constexpr auto dd = sys_days(t1) - sys_days(t0);
+				static_assert(dd == std::chrono::days(367));
+				constexpr auto yy = years(dd.count() / 365.);
+				static_assert(y3 == yy);
+			}
+
+			return 0;
+		}
+#endif // _DEBUG
+	}
+
+	// Return true if date is a holiday.
+	using holiday = bool(*)(const ymd&);
+	namespace holidays {
+
+		// Yearly holiday on month and day
+		constexpr bool month_day(const ymd& d, const std::chrono::month& month, const std::chrono::day& day)
+		{
+			return (d.month() == month) and (d.day() == day);
+		}
+
+		constexpr bool new_year_day(const ymd& d)
+		{
+			return month_day(d, std::chrono::January, std::chrono::day(1));
+		}
+
+		// ...
+
+		constexpr bool christmas_day(const ymd& d)
+		{
+			return month_day(d, std::chrono::December, std::chrono::day(25));
+		}
+
+	} // namespace holiday
+
+	// Return true on non-trading days.
+	using calendar = bool(*)(const ymd&);
+	namespace calendars {
+
+		constexpr bool weekday(const ymd& d)
+		{
+			auto w = std::chrono::year_month_weekday(d);
+			auto wd = w.weekday();
+
+			return (wd == std::chrono::Saturday) or (wd == std::chrono::Sunday);
+		}
+		constexpr bool example(const ymd& d)
+		{
+			return weekday(d) or holidays::new_year_day(d);
+		}
+
+		// constexpr bool nyse(const ymd& d) ...
+
+	} // namespace calendars
+
+	// Roll to business day conventions.
+	enum class roll {
+		none,
+		following, // following business day
+		previous,  // previous business day
+		modified_following, // following business day unless different month
+		modified_previous,  // previous business day unless different month
+	};
+
+	constexpr ymd adjust(const ymd& date, roll convention, const calendar& cal = calendars::weekday)
 	{
-		std::chrono::year_month_day t0{d0};
-		std::chrono::year_month_day t1{d1};
-
-		int dy = (int)t1.year() - (int)t0.year();
-		int dm = (unsigned)t1.month() - (unsigned)t0.month();
-		int dd0 = (unsigned)t0.day();
-		int dd1 = (unsigned)t1.day();
-		if (dd0 == 31) {
-			dd0 = 30;
+		if (!cal(date)) {
+			return date;
 		}
-		if (dd1 == 31 and dd0 > 29) {
-			dd1 = 30;
-		}
-		int dd = dd1 - dd0;
 
-		return dy + dm / 12. + dd / 360.;
+		switch (convention) {
+		case roll::none:
+			return date;
+		case roll::previous:
+			return adjust(sys_days(date) - std::chrono::days(1), convention, cal);
+		case roll::following:
+			return adjust(sys_days(date) + std::chrono::days(1), convention, cal);
+		case roll::modified_following:
+		{
+			const auto date_ = adjust(sys_days(date), roll::following, cal);
+			return date_.month() == date.month()
+				? date_
+				: adjust(date, roll::previous, cal);
+		}
+		case roll::modified_previous:
+		{
+			const auto date_ = adjust(sys_days(date), roll::previous, cal);
+			return date_.month() == date.month()
+				? date_
+				: adjust(date, roll::following, cal);
+		}
+		default:
+			return ymd{};
+		}
+
+		return date;
+	}
+
+	enum class frequency {
+		annually = 1,
+		semiannually = 2,
+		quarterly = 4,
+		monthly = 12,
+	};
+	constexpr auto tenor(frequency f)
+	{
+		return 12 / (int)f;
 	}
 
 #ifdef _DEBUG
-
-#pragma warning(push)
-#pragma warning(disable: 4455)
-	inline int dcf_30_360_test()
+	int test()
 	{
-		using std::chrono::sys_days;
-		using std::literals::chrono_literals::operator""y;
+		using year = std::chrono::year;
+		using days = std::chrono::sys_days;
+		//using seconds = std::chrono::seconds;
 		{
-			constexpr auto t0{ 2023y / 1 / 1 };
-			constexpr auto t1{ 2023y / 1 / 2 };
-			static_assert(dcf_30_360(t0, t0) == 0);
-			static_assert(dcf_30_360(t0, t1) == 1 / 360.);
-			static_assert(dcf_30_360(2023y / 1 / 1, 2023y / 2 / 1 ) == 1 / 12.);
-			static_assert(dcf_30_360(2023y / 1 / 31, 2023y / 2 / 1 ) == 1 / 12. - 29 / 360.);
-			static_assert(dcf_30_360(1985y / 1 / 31, 1986y / 3 / 16) == 406 / 360.);
-			//!!! more tests
+			constexpr auto d0 = year{ 2023 } / 1 / 1;
+			constexpr auto d1 = year{ 2023 } / 1 / 2;
+			constexpr auto dt = days(d1) - days(d0);
+			constexpr auto dy = years(dt);
+			static_assert(0 != dy.count());
+			static_assert(1 == dt.count());
+			constexpr auto y0 = dcf::_years(d0, d1);
+			static_assert(dy == y0);
 		}
 
 		return 0;
 	}
-#pragma warning(pop)
-
 #endif // _DEBUG
 
-}
+} // namespace fms::date
