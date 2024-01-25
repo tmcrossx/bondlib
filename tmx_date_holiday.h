2867817d2aa6ad0cd2bc65470d00402c13522306
Tianxin Zhang <tz2402@nyu.edu>
2024-01-25 10:12:14 -0500

    Add more holidays in tmx_date_holiday.h and tmx_date_holiday_calendar.h

--- tmx_date_holiday.h
+++ tmx_date_holiday.h
@@ -10,7 +10,6 @@ namespace tmx::date::holiday {
 	constexpr bool weekend(const date::ymd& d)
 	{
 		auto wd = std::chrono::year_month_weekday(d).weekday();
-
 		return (wd == std::chrono::Saturday) || (wd == std::chrono::Sunday);
 	}
 
@@ -53,37 +52,101 @@ namespace tmx::date::holiday {
 	//constexpr bool good_friday(const date::ymd& d);
 
 	// Last Monday in May.
-	//constexpr bool memorial_day(const date::ymd& d);
+	constexpr bool memorial_day(const date::ymd& d)
+	{
+		return last_weekday(d, std::chrono::weekday_last(std::chrono::Monday));
+	}
 
 	// Juneteenth National Independence Day
-	//constexpr bool juneteenth(const date::ymd& d);
+	constexpr bool juneteenth(const date::ymd& d)
+	{
+		return month_day(d, std::chrono::June, std::chrono::day(19));
+	}
 
 	// July 4. Roll to Monday if weekend.
-	//constexpr bool independence_day(const date::ymd& d);
+	constexpr bool independence_day(const date::ymd& d)
+	{
+		auto [y1, m1, d1] = from_ymd(d);
+		auto wd = std::chrono::year_month_weekday(to_ymd(y1,7,4)).weekday();
+		if (wd == std::chrono::Saturday) {
+			return month_day(d, std::chrono::July, std::chrono::day(3));
+		}
+		else if (wd == std::chrono::Sunday) {
+			return month_day(d, std::chrono::July, std::chrono::day(5));
+		}
+		else{
+			return month_day(d, std::chrono::July, std::chrono::day(4));
+		}
+	}
 	
 	// First Monday of September Labor Day
-	//constexpr bool memorial_day(const date::ymd& d);
+	constexpr bool labor_day(const date::ymd& d)
+	{
+		return nth_weekday(d, std::chrono::September, std::chrono::Monday, 1);
+	}
 	
 	// Second Monday of October
-	//constexpr bool columbus_day(const date::ymd& d);
+	constexpr bool columbus_day(const date::ymd& d)
+	{
+		return nth_weekday(d, std::chrono::October, std::chrono::Monday, 2);
+	}
 	
 	// November 11. If it falls on Saturday, the preceding Friday. If it falls on Sunday, the following Monday.
-	//constexpr bool veterans_day(const date::ymd& d);
+	constexpr bool veterans_day(const date::ymd& d)
+	{
+		auto [y1, m1, d1] = from_ymd(d);
+		auto wd = std::chrono::year_month_weekday(to_ymd(y1, 11, 11)).weekday();
+		if (wd == std::chrono::Saturday) {
+			return month_day(d, std::chrono::November, std::chrono::day(10));
+		}
+		else if (wd == std::chrono::Sunday) {
+			return month_day(d, std::chrono::November, std::chrono::day(12));
+		}
+		else {
+			return month_day(d, std::chrono::November, std::chrono::day(11));
+		}
+	}
 	
 	// Fourth Thursday of November
-	//constexpr bool thanksgiving(const date::ymd& d);
+	constexpr bool thanksgiving(const date::ymd& d)
+	{
+		return nth_weekday(d, std::chrono::November, std::chrono::Thursday, 4);
+	}
 
 	// December 25. Roll to Monday if weekend.
 	constexpr bool christmas_day(const date::ymd& d)
 	{
-		return month_day(d, std::chrono::December, std::chrono::day(25));
+		auto [y1, m1, d1] = from_ymd(d);
+		auto wd = std::chrono::year_month_weekday(to_ymd(y1, 12, 25)).weekday();
+		if (wd == std::chrono::Saturday) {
+			return month_day(d, std::chrono::December, std::chrono::day(24));
+		}
+		else if (wd == std::chrono::Sunday) {
+			return month_day(d, std::chrono::December, std::chrono::day(26));
+		}
+		else {
+			return month_day(d, std::chrono::December, std::chrono::day(25));
+		}
 	}
 
 #ifdef _DEBUG
 	static_assert(new_year_day(to_ymd(2021, 1, 1)));
 	static_assert(martin_luther_king_day(to_ymd(2024, 1, 15)));
 	//  TODO: Add more holiday tests
-	static_assert(christmas_day(to_ymd(2021, 12, 25)));
+	static_assert(memorial_day(to_ymd(2021, 5, 31)));
+	static_assert(juneteenth(to_ymd(2021, 6, 19)));
+	static_assert(independence_day(to_ymd(2023, 7, 4)));
+	static_assert(independence_day(to_ymd(2020, 7, 3)));
+	static_assert(independence_day(to_ymd(2021, 7, 5)));
+	static_assert(labor_day(to_ymd(2021, 9, 6)));
+	static_assert(columbus_day(to_ymd(2021, 10, 11)));
+	static_assert(veterans_day(to_ymd(2021, 11, 11)));
+	static_assert(veterans_day(to_ymd(2023, 11, 10)));
+	static_assert(thanksgiving(to_ymd(2021, 11, 25)));
+	static_assert(christmas_day(to_ymd(2021, 12, 24)));
+	static_assert(christmas_day(to_ymd(2022, 12, 26)));
+	static_assert(christmas_day(to_ymd(2023, 12, 25)));
+
 #endif // _DEBUG
 
 } // namespace tmx::date::holiday
--- tmx_date_holiday_calendar.h
+++ tmx_date_holiday_calendar.h
@@ -9,11 +9,17 @@ namespace tmx::date::holiday::calendar {
 
 	// https://www.sifma.org/resources/general/holiday-schedule/#us
 	constexpr bool NYSE(const ymd& d)
-	{
+	{	// TODO: Add more holidays
 		return weekend(d)
 			|| holiday::new_year_day(d)
 			|| holiday::martin_luther_king_day(d)
-			// TODO: Add more holidays
+			|| holiday::memorial_day(d)
+			|| holiday::juneteenth(d)
+			|| holiday::independence_day(d)
+			|| holiday::labor_day(d)
+			|| holiday::columbus_day(d)
+			|| holiday::veterans_day(d)
+			|| holiday::thanksgiving(d)
 			|| holiday::christmas_day(d);
 	}
 
