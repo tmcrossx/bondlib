e54399a267fad66a95b873cfbbcdc2885c72d122
Keith A. Lewis <kal@kalx.net>
2024-07-14 12:37:17 -0400

    treasury bond instrument is 2-row array

---
+++ instrument/tmx_instrument_bond_treasury.h
@@ -0,0 +1,25 @@
+// tmx_instrument_bond_treasury.h - Treasury instruments
+#pragma once
+#include "tmx_instrument_bond.h"
+
+namespace tmx::instrument::bond {
+
+	// class bill
+	// class note
+	template<class C = double, class F = double>
+	struct treasury : public tmx::instrument::bond::basic<C,F> {
+		treasury(date::ymd dated, date::ymd maturity, C coupon, F face = 100)
+			: tmx::instrument::bond::basic<C, F>{ 
+				dated, 
+				maturity, 
+				coupon, 
+				date::frequency::semiannually, 
+				date::day_count_isdaactualactual, 
+				date::business_day::roll::following, 
+				date::holiday::calendar::FED, 
+				face 
+			}
+		{ }
+	};
+
+} // namespace tmx::instrument::treasury
