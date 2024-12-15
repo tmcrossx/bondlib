afa85bad4cd740396f12d6b3adc439cf404be985
Keith A. Lewis <kal@kalx.net>
2024-12-14 23:22:23 -0500

    split bond cash flows into interest and principal

--- security/tmx_bond.h
+++ security/tmx_bond.h
@@ -31,9 +31,9 @@ namespace tmx::security {
 		schedule<F> call;
 	};
 
-	// Return instrument cash flows for basic bond from present value date.
+	// Return instrument interest cash flows for basic bond from present value date.
 	template<class C = double, class F = double>
-	inline auto instrument(const bond<C, F>& bond, const date::ymd& pvdate)
+	inline auto interest(const bond<C, F>& bond, const date::ymd& pvdate)
 	{
 		using namespace fms::iterable;
 		using namespace tmx::date;
@@ -51,13 +51,26 @@ namespace tmx::security {
 		const auto dcf = delta(prepend(d0, apd), bond.day_count);
 		// cash flows
 		auto c = constant(bond.face * bond.coupon) * dcf;
-		// face value at maturity
-		const auto u_ = append(u, diffyears(bond.maturity, pvdate));
-		const auto c_ = append(c, bond.face);
 
-		return instrument::iterable(u_, c_);
+		return instrument::iterable(u, c);
 	}
+	// Return instrument principal cash flows for basic bond from present value date.
+	template<class C = double, class F = double>
+	inline auto principal(const bond<C, F>& bond, const date::ymd& pvdate)
+	{
+		const auto u = date::diffyears(bond.maturity, pvdate);
+		const auto c = bond.face;
 
+		return instrument::iterable(single(u), single(c));
+	}
+	// Return all cash flows for basic bond from present value date.
+	template<class C = double, class F = double>
+	inline auto instrument(const bond<C, F>& bond, const date::ymd& pvdate)
+	{
+		using namespace fms::iterable;
+
+		return merge(interest(bond, pvdate), principal(bond, pvdate));
+	}
 #ifdef _DEBUG
 
 	inline int bond_test()
@@ -70,8 +83,16 @@ namespace tmx::security {
 		auto d = 2023y / 1 / 1;
 		bond<> b0{ d, d + years(10), 0.05 };
 		{
+			auto p = principal(b0, d);
+			assert(1 == size(p));
+			const auto uc = *p;
+			assert(uc.u == diffyears(b0.maturity, d));
+			assert(uc.c == 100);
+			assert(!++p);
+		}
+		{
 			bond b(d, d + months(6), 0.05);
-			auto i = instrument(b, d);
+			auto i = interest(b, d);
 			auto i2 = i;
 			assert(i == i2);
 			i = i2;
@@ -85,21 +106,23 @@ namespace tmx::security {
 			assert(!i);
 		}
 		{
-			auto i = instrument(b0, d);
-			assert(21 == size(i));
+			auto i = interest(b0, d);
+			assert(20 == size(i));
 			auto c0 = *i;
 			assert(c0.c == 2.5);
 			assert(c0.u == diffyears(d + months(period(b0.frequency)), d));
 
-			i = drop(i, 20);
+			i = drop(i, 19);
 			auto cn = *i;
 			assert(cn.u == diffyears(b0.maturity, d));
-			assert(cn.c == 100);
+			assert(cn.c == 2.5);
+
+			assert(!++i);
 		}
 		{
 			auto pvdate = d + months(1);
-			auto i = instrument(b0, pvdate);
-			assert(21 == size(i));
+			auto i = interest(b0, pvdate);
+			assert(20 == size(i));
 			auto c0 = *i;
 			assert(c0.c < 2.5);
 			assert(c0.u == diffyears(b0.dated + months(period(b0.frequency)), pvdate));
@@ -107,15 +130,16 @@ namespace tmx::security {
 			++i;
 			auto c1 = *i;
 			assert(c1.c == 2.5);
-			i = drop(i, 19);
+			i = drop(i, 18);
 			auto cn = *i;
 			assert(cn.u == diffyears(b0.maturity, pvdate));
-			assert(cn.c == 100);
+			assert(cn.c == 2.5);
+			assert(!++i);
 		}
 		{
 			auto pvdate = d - months(1);
-			auto i = instrument(b0, pvdate);
-			assert(21 == size(i));
+			auto i = interest(b0, pvdate);
+			assert(20 == size(i));
 			auto c0 = *i;
 			assert(c0.c == 2.5); // accrue from dated date
 			assert(c0.u == diffyears(b0.dated + months(period(b0.frequency)), pvdate));
@@ -123,10 +147,10 @@ namespace tmx::security {
 			++i;
 			auto c1 = *i;
 			assert(c1.c == 2.5);
-			i = drop(i, 19);
+			i = drop(i, 18);
 			auto cn = *i;
 			assert(cn.u == diffyears(b0.maturity, pvdate));
-			assert(cn.c == 100);
+			assert(cn.c == 2.5);
 		}
 
 		return 0;
